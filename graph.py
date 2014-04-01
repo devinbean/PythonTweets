74c10a8f755c6975531d792799c945fe9d72db66
devinbean <devinbean@Devins-MacBook-Air.local>
2014-04-01 07:48:23 +0100

    final

--- graph.py
+++ graph.py
@@ -6,6 +6,8 @@ from itertools import islice
 import matplotlib.figure as fg
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.dates import date2num
+
 
 def take(n, iterable):
     "Return first n items of the iterable as a list"
@@ -23,6 +25,7 @@ flatdates = []
 tweetlist = []
 finaldate = []
 finaltweet = []
+reworduse = []
 
 W = raw_input('Search term: ')
 
@@ -55,26 +58,29 @@ texttweet = flatten(tweetlist)
 for all in texttweet:
     finaltweet.append(all)
 
+finaltweet.pop(0)
+
 counts = Counter()
 
 tentweets = [finaltweet[x:x+10] for x in xrange(0, len(finaltweet),10)]
 tendate = finaldate[0::10]
-    
 for list in tentweets:
     for x in list:
         counts.update(words.findall(x.lower()))
     worduse.append(counts[W])
     counts.clear()
-
+    #for all in reversed(worduse):
+#reworduse.append(all)
 x = range(len(tendate))
-
-print tendate
-
-#plt.plot(x, worduse)
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot(111)
+plt.bar(x, worduse, width=0.4)
 #plt.xticks(np.arange(min(x), max(x)+1, 2.0), tendate, rotation='vertical')
-#plt.ylabel('Word Frequency')
-#plt.title('"%s" Frequency vs. Date' %W, fontsize=12)
-#plt.tight_layout()
-#   plt.show()
+plt.xticks(x, tendate, rotation='vertical')
+plt.ylabel('Word Frequency')
+plt.setp(ax.get_xticklabels()[::2], visible=False)
+plt.title('"%s" Frequency vs. Date' %W, fontsize=12)
+plt.tight_layout()
+plt.show()
 
 
